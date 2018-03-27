a4a42fb310df3da72244144b42b000d13114a02e
Elvis Shi <shonetesmith@gmail.com>
2018-03-27 18:24:18 +0900

    Update popteamepic.rb

--- popteamepic.rb
+++ popteamepic.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
- class PopTeamEpic < Formula
+ class Popteamepic < Formula
   desc "TAKESHOBO!? Shouldn't it be destroyed..."
   homepage "https://github.com/el-hoshino/popteamepic"
   url "https://github.com/el-hoshino/popteamepic/archive/0.1.tar.gz"
